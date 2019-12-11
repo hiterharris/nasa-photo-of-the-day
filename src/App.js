43a1106e929d2cc507dc4c1ef6b889020a6c2507
Hiter Harris <hiterharris@comcast.net>
2019-12-11 15:14:07 -0500

    set up webpage, imported nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React from 'react';
+import Nav from './components/Nav';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+    <div className='App'>
+      <Nav />
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,2 +1,17 @@
-// Navigation Menu Component
-// Exported to App
+import React from 'react';
+import logo from '../images/nasa-logo.svg';
+import '../App.css';
+
+function Nav() {
+  return (
+    <div className='Nav nav-menu'>
+        <img className="logo" src={logo} />
+        <h1><a href='https://www.nasa.gov/' target='_blank'>Home</a></h1>
+        <h1><a href='https://www.nasa.gov/about/index.html' target='_blank'>About</a></h1>
+        <h1><a href='https://www.nasa.gov/multimedia/imagegallery/iotd.html' target='_blank'>Gallery</a></h1>
+        <h1><a href='https://www.nasa.gov/multimedia/nasatv/index.html#public' target='_blank'>NASA TV</a></h1>
+    </div>
+  );
+}
+
+export default Nav;
